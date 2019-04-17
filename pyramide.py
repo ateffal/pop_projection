f70cff904048833b8b33cc14a49374d3bb66faed
Amine TEFFAL <a.teffal@gmail.com>
2019-04-17 12:42:34 +0100

    minor modification

--- pyramide.py
+++ pyramide.py
@@ -9,26 +9,10 @@ from pop_projection import Effectifs as eff
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-def law_replacement1(departures_, year_):
-    
-    '''
-        assumes departures_ is a dic storing number of departures by group of the year year_
-        returns a list of dics having keys : sex, age, number and group
-        
-    '''
-    new_employees = []
-
-    for g in departures_:
-        temp = {'sex':'male', 'age' : 30, 'number': 0.25 * departures_[g],'group':g}
-        new_employees.append(temp)
-        temp = {'sex':'female', 'age' : 21, 'number': 0.25 * departures_[g],'group':g}
-        new_employees.append(temp)
-    
-    return new_employees
+from pop_projection import sample_laws as sl
 
 # Path for input data
-path ="./pop_projection/data/"
+path = "./pop_projection/data/"
 
 # Number of years to project
 MAX_ANNEES = 100
@@ -40,7 +24,8 @@ children = pd.read_csv(path + "children.csv",sep=";", decimal = ",")
 
 
 # Projection of population
-numbers_ = eff.simulerEffectif(employees, spouses, children, 'TV 88-90', MAX_ANNEES, law_replacement_ = law_replacement1)
+numbers_ = eff.simulerEffectif(employees, spouses, children, 'TV 88-90', MAX_ANNEES, 
+                               law_replacement_ = sl.law_replacement1, law_marriage_=sl.law_mar1)
 
 ind_emp_numbers = eff.individual_employees_numbers(numbers_[0])
 
@@ -48,34 +33,30 @@ ind_emp_numbers[0].to_csv("Employees_lives.csv", sep=';')
 
 employees_proj = ind_emp_numbers[0]
 
-#Diplaying some data
-# print(employees_proj.head(5))
-
-
 year = 'year_'
 
-for i in range(1, MAX_ANNEES//10):
+for i in range(1, MAX_ANNEES // 10):
     
-    emp_grouped = employees_proj.groupby(['age','sex'], as_index=False)[year+str((i-1)*10)].sum()
+    emp_grouped = employees_proj.groupby(['age','sex'], as_index=False)[year + str((i - 1) * 10)].sum()
     
     max_age = 100 # 101
     
     min_age = 19
     
     #update colum age to be age at year (i-1)*10
-    emp_grouped['age'] = emp_grouped['age'] - MAX_ANNEES + (i-1)*10
+    emp_grouped['age'] = emp_grouped['age'] - MAX_ANNEES + (i - 1) * 10
     
     emp_grouped = emp_grouped.loc[(emp_grouped['age'] < max_age) & (emp_grouped['age'] > min_age)]
     
-    table = pd.pivot_table(emp_grouped, values=year+str((i-1)*10), index=['age'],  columns=['sex'], aggfunc=np.sum)
+    table = pd.pivot_table(emp_grouped, values=year + str((i - 1) * 10), index=['age'],  columns=['sex'], aggfunc=np.sum)
     
     table = table.fillna(0)
     
     #calculate percentage
     if 'male' in list(table.columns):
-        table['male'] = table['male']/np.sum(table['male'])
+        table['male'] = table['male'] / np.sum(table['male'])
     if 'female' in list(table.columns):
-        table['female'] = table['female']/np.sum(table['female'])
+        table['female'] = table['female'] / np.sum(table['female'])
     
     if 'female' in list(table.columns):
         table['female'] = table['female'] * (-1)
@@ -83,29 +64,20 @@ for i in range(1, MAX_ANNEES//10):
     plt.subplot(4 , 4, i)
     plt.subplots_adjust(hspace = 0.5)
     plt.xlim(-0.05,0.05)
-    plt.title('Year ' + str((i-1)*10))
+    plt.title('Year ' + str((i - 1) * 10))
     
     if 'male' in list(table.columns):
         values = [0] * (max_age - min_age - 1)
         for i in range(len(table['male'])):
             values[table.index[i] - min_age - 1] = table.iloc[i]['male']
-        p_male = plt.barh(list(range(min_age + 1, max_age)), values, color = (149/255,125/255,98/255) )
+        p_male = plt.barh(list(range(min_age + 1, max_age)), values, color = (149 / 255,125 / 255,98 / 255))
     
     if 'female' in list(table.columns):
         for i in range(len(table['female'])):
             values[table.index[i] - min_age - 1] = table.iloc[i]['female']
-        p_female = plt.barh(list(range(min_age + 1, max_age)), values, color = (0/255,128/255,0/255))
+        p_female = plt.barh(list(range(min_age + 1, max_age)), values, color = (0 / 255,128 / 255,0 / 255))
 
     plt.figlegend((p_male[0], p_female[0]), ('Men', 'Women'), loc = 'lower center')
     plt.suptitle('Evolution of Ages Pyramid')
 
 plt.show()
-
-
-
-
-
-
-
-
-
