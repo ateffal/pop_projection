6da38547bd76f62c2d27ead70f8be083ad2092f0
ateffal <a.teffal@gmail.com>
2019-05-18 16:36:19 +0000

    updated example_3

--- example_3.py
+++ example_3.py
@@ -10,6 +10,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def salaries_new_emp_1(new_employees):
+    """ 
+        Returns a data frame containing for each new employee his salary the he entered 
+        the population.
+  
+        Parameters: 
+            new_employees (DataFrame): a DataFrame containing all column data for new employees 
+            plus a column entrance (the year this new employee entered the population).
+        
+        Returns: 
+            DataFrame: A DataFrame containing projected salaries for each new employee.
+    """
+
+    ids = list(new_employees['id'])
+    salaries = [0]*len(ids)
+
+    i = 0
+    for e in list(new_employees['entrance']):
+        salaries[i] = 1000 * (1+0.02)**e
+        i = i+1
+
+    temp_df = pd.DataFrame()
+
+    temp_df['id'] = ids
+    temp_df['salary'] = salaries
+
+    return temp_df
 
 
 # Define law of replacement
@@ -20,8 +47,6 @@ def law_replacement1(departures_, year_):
         returns a list of dics having keys : key, number and data
         
     '''
-
-    print(departures_)
 
     def nouveaux(g_):
         structure_nouveaux = {'1':[25,25,0.8],'2':[25,25,0.8],'3':[25,25,0.6],'4':[29,29,0.6],'5':[28,28,0.5,],
@@ -44,18 +69,18 @@ def law_replacement1(departures_, year_):
     new_employees = []
 
     for g in departures_:
+        if departures_[g] > 0 :
+            # add a male
+            if nouveaux(g)[2] > 0:
+                temp = {'key':'male_groupe_' + str(g) + '_year_' + str(year_), 
+                'number':nouveaux(g)[2]*departures_[g]*taux_rempl(year_, g),'data':['active', 'male', 'not married', nouveaux(g)[0], year_,g,'01/01/'+str((2018+year_+1)),'31/12/'+str((2018+year_-nouveaux(g)[0]))]}
+                new_employees.append(temp)
 
-        # add a male
-        if nouveaux(g)[2] > 0:
-            temp = {'key':'male_groupe_' + str(g) + '_year_' + str(year_), 
-            'number':nouveaux(g)[2]*departures_[g]*taux_rempl(year_, g),'data':['active', 'male', 'not married', nouveaux(g)[0], year_,g,'01/01/'+str((2018+year_+1)),'31/12/'+str((2018+year_-nouveaux(g)[0]))]}
-            new_employees.append(temp)
-
-        # add a female
-        if nouveaux(g)[2] < 1:
-            temp = {'key':'female_groupe_' + str(g) + 'year_' + str(year_), 
-            'number':(1-nouveaux(g)[2])*departures_[g]*taux_rempl(year_, g),'data':['active', 'female', 'not married', nouveaux(g)[1], year_,g,'01/01/'+str((2018+year_+1)),'31/12/'+str((2018+year_-nouveaux(g)[1]))]}
-            new_employees.append(temp)
+            # add a female
+            if nouveaux(g)[2] < 1:
+                temp = {'key':'female_groupe_' + str(g) + 'year_' + str(year_), 
+                'number':(1-nouveaux(g)[2])*departures_[g]*taux_rempl(year_, g),'data':['active', 'female', 'not married', nouveaux(g)[1], year_,g,'01/01/'+str((2018+year_+1)),'31/12/'+str((2018+year_-nouveaux(g)[1]))]}
+                new_employees.append(temp)
     
     return new_employees
 
@@ -67,19 +92,95 @@ employees = pd.read_csv(path + "employees.csv",sep=";", decimal = ",")
 spouses = pd.read_csv(path + "spouses.csv",sep=";", decimal = ",")
 children = pd.read_csv(path + "children.csv",sep=";", decimal = ",")
 
+# Loading salaries
+salaries = pd.read_csv(path + "salaries.csv",sep=";", decimal = ",")
+
 
 # Projection of population
 # Number of years to project
-MAX_ANNEES = 3
+MAX_ANNEES = 20
 
 # Projection
 numbers_ = eff.simulerEffectif(employees, spouses, children, 'TV 88-90', MAX_ANNEES, law_replacement_ = law_replacement1)
 
-# Getting individual numbers projection for employees
-lives, deaths, resignation, type_ = eff.individual_employees_numbers(numbers_[0])
 
-# printing new employees
-df = eff.new_employees(numbers_[0], MAX_ANNEES)
-print(df.tail(10))
+def project_contributions(projected_salaries, rate_contribution, MAX_YEARS):
+
+    return None
+
+
+def project_salaries(employees_proj_, salaries, MAX_YEARS, salaries_new_emp = None, sal_evol = 0):
+    """ 
+        Returns a DataFrame containing annual projection of the salaries.
+  
+        Parameters: 
+            employees_proj_ (dic): a dic containing projected employees in the form of those 
+                                   returned by simulerEffectif.
+                                   
+            salaries (DataFrame) : A DataFrame containing for each employee (including new employees if 
+                                   salaries_new_emp is None ) his salary at year 0 (at year of entrance 
+                                   if it's a new employee).
+
+            MAX_YEARS (int): Number of years of projection.
+
+        Optional parameters:
+            salaries_new_emp (function) : A function accepting as parameter a DataFrame containing all 
+                                          columns data for new employees plus a column 'entrance' (the year this new 
+                                          employee entered  the population) and returning a DataFrame containing 
+                                          projected salaries for each new employee.
+
+        sal_evol (numeric) : annual increase of the salaire. Salary at year i+1 = (Salary at year i)*(1+sal_evol)
+        
+        Returns: 
+            DataFrame: A DataFrame containing projected salaries for each employee
+    """
+
+    # Getting new employees
+    df_new = eff.new_employees(numbers_[0], MAX_YEARS)
+
+    # if salaries_new_emp is not None, get salaries of new employees and add them to salaries
+    if not salaries_new_emp is None:
+        salaries_new_emp_df = salaries_new_emp_1(df_new )
+        salaries = pd.concat([salaries,salaries_new_emp_df] )
+
+    print(salaries)
+
+    # Getting individual projected numbers for employees
+    lives, deaths, resignation, type_ = eff.individual_employees_numbers(employees_proj_)
+
+    # Join lives and salaries
+    df_temp = lives.join(salaries.set_index('id'),on='id', how='inner', lsuffix='_lives', rsuffix='_salaries')
+
+    # Get years columns
+    years_columns = [c  for c in df_temp.columns if c.startswith('year_')]
+
+    type_ = type_.set_index('id')
+
+    # Get the number of rows
+    n = len(df_temp)
+
+    # Project salary
+    for i in range(n):
+        for c in years_columns:
+            # year
+            y = int(c[5:])
+            # id 
+            id_ = df_temp.loc[i,'id']
+            # salaries are not zero for actives only
+            if type_.loc[id_, c] == 'active':
+                df_temp.loc[i, c] = df_temp.loc[i, c] * df_temp.loc[i, 'salary']*((1+sal_evol)**y)
+            else:
+                df_temp.loc[i, c] = 0
+
+
+
+    return df_temp[df_temp.columns[:-1]]
+
+
+df = project_salaries(numbers_[0],salaries, MAX_ANNEES, salaries_new_emp_1, 0.035)
+# print(df)
+
+df.to_csv('asupprimer.csv', sep=';', index=False, decimal=',')
+
 
 
