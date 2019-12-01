4665f8de4e22213d5bc0a00c970c2ee4efd7ec39
ateffal <a.teffal@gmail.com>
2019-12-01 19:23:32 +0100

    init* functions updated to handel numbers

--- pop_projection/tools.py
+++ pop_projection/tools.py
@@ -17,19 +17,25 @@ def init_employees_proj(employees, MAX_YEARS):
     # - type (list of string) : if employee is still active at year i, then element i is 'active' otherwise 'retired'
     employees_proj = {}
     n_e = len(employees)
+
     
     for i in range(n_e):
         if employees["type"][i] == "active":
             employees_proj[employees["id"][i]] = {'data':dict(zip(employees.columns[1:],list(employees.iloc[i])[1:])), 'exist':1, 
-                'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'res':[0]*MAX_YEARS, 'type':['active'] + ['']*(MAX_YEARS-1)}  
+                'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'res':[0]*MAX_YEARS, 
+                'type':['active'] + ['']*(MAX_YEARS-1), 'numbers':[1] + [0]*(MAX_YEARS-1), 'spouses_number':0, 'children_number':0}  
         else:
             employees_proj[employees["id"][i]] = {'data':dict(zip(employees.columns[1:],list(employees.iloc[i])[1:])), 'exist':1, 
-                'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'res':[0]*MAX_YEARS, 'type':['retired']*MAX_YEARS}
+                'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'res':[0]*MAX_YEARS, 
+                'type':['retired']*MAX_YEARS, 'numbers':[1] + [0]*(MAX_YEARS-1), 'spouses_number':0, 'children_number':0}
         employees_proj[employees["id"][i]]['data']['age0'] = employees["age"][i]
+        if 'number' in list(employees.columns[1:]):
+            employees_proj[employees["id"][i]]['numbers'][0] = employees["number"][i]
+    
 
     return employees_proj
 
-def init_spouses_proj(spouses, MAX_YEARS):
+def init_spouses_proj(spouses, MAX_YEARS, employees_proj_ = None):
     
     #projected spouses are stored in a dic with keys:
     # - data : all columns of dataframe employees passed to simulerEffectif except column id
@@ -43,12 +49,18 @@ def init_spouses_proj(spouses, MAX_YEARS):
 
     for i in range(n_s):
         spouses_proj[(spouses["id"][i], spouses["rang"][i])] = {'data':dict(zip(spouses.columns[2:],list(spouses.iloc[i])[2:])), 'exist':1, 
-            'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'rev' : [0]*MAX_YEARS,'type':[spouses["type"][i]] + ['']*(MAX_YEARS-1)}
+            'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'rev' : [0]*MAX_YEARS,
+            'type':[spouses["type"][i]] + ['']*(MAX_YEARS-1), 'numbers':[1] + [0]*(MAX_YEARS-1)}
         spouses_proj[(spouses["id"][i], spouses["rang"][i])]['data']['age0'] = spouses["age"][i]
+        if 'number' in list(spouses.columns[1:]):
+            spouses_proj[spouses["id"][i]]['numbers'][0] = spouses["number"][i]
+        if not (employees_proj_ is None):
+            if spouses["type"][i] !='widow':
+                employees_proj_[spouses["id"][i]]['spouses_number'] +=1
 
     return spouses_proj
 
-def init_children_proj(children, MAX_YEARS):
+def init_children_proj(children, MAX_YEARS, employees_proj_ = None):
     
     #projected spouses are stored in a dic with keys:
     # - data : all columns of dataframe employees passed to simulerEffectif except column id
@@ -62,8 +74,14 @@ def init_children_proj(children, MAX_YEARS):
 
     for i in range(n_c):
         children_proj[(children["id"][i], children["rang"][i])] = {'data':dict(zip(children.columns[2:],list(children.iloc[i])[2:])), 'exist':1, 
-            'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 'type':[children["type"][i]] + ['']*(MAX_YEARS-1)}
+            'entrance':0, 'lives':[1] + [0]*(MAX_YEARS-1), 'deaths' : [0]*MAX_YEARS, 
+            'type':[children["type"][i]] + ['']*(MAX_YEARS-1), 'numbers':[1] + [0]*(MAX_YEARS-1)}
         children_proj[(children["id"][i], children["rang"][i])]['data']['age0'] = children["age"][i]
+        if 'number' in list(children.columns[1:]):
+            children_proj[children["id"][i]]['numbers'][0] = children["number"][i]
+        if not (employees_proj_ is None):
+            if children["type"][i]!='widow':
+                employees_proj_[children["id"][i]]['children_number'] +=1
 
     return children_proj
     
