08efdec591148c9d166c9207170b3ceb70a995d8
Amine TEFFAL <a.teffal@gmail.com>
2021-12-13 11:57:04 +0100

    Change version to 3.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pop_projection",
-    version="2.2.3",
+    version="3.0.0",
     author="Amine TEFFAL",
     author_email="a.teffal@gmail.com",
     description="Projection of population of a retirement plan.",
